83171dade554f4322c1f9113984e7408ad98f9f6
absyinka <absyinka@gmail.com>
2023-08-24 13:13:14 +0100

    small fix

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using DotnetConsoleApp.Menu;
 using DotnetConsoleApp.Repository;
 using DotnetConsoleApp.Service;
 using DotnetConsoleApp.Shared;
+
 // Be able to Create an employee record
 // Be able to update an employee record
 // Be able to find employee by employee code
@@ -12,18 +13,13 @@ using DotnetConsoleApp.Shared;
 
 
 // DTO - Data Tranfer Object
-
 // A class can inherit (or extend) another class, 
 // A class can also implement more than one interface
 
 
-
 // int[] number = { 0, 1, 5, 7, 10 };
-
 // var lastIndex = number.Length - 1;
-
 // var lastValue = number[number.Length - 1];
-
 // Console.WriteLine($"Last Index: {lastIndex}");
 // Console.WriteLine($"Last Value: {lastValue}");
 
